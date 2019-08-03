55397493823efe64e6efd148b8faf808b6dc505a
shunjid <shunjid1862@gmail.com>
2019-08-03 18:36:32 +0600

    initiated Home API

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,9 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NoResume.Models;
 
@@ -6,9 +11,41 @@ namespace NoResume.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public HomeController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult Index(IFormCollection formFields)
+        {
+            List<Object> objectList = new List<object>();
+            var developerId = _userManager.FindByNameAsync(formFields["developerUsername"]).Result.Id;
+            var shortBios = _context.ShortBios.FindAsync(developerId);
+            var workingProfile = _context.WorkingProfiles.FindAsync(developerId);
+            
+            objectList.Add(developerId);
+            objectList.Add(shortBios);
+            objectList.Add(workingProfile);
+            
+            return Json(objectList);
+        }
+        
+        [HttpGet("Home/Dev/{username}")]
+        public async Task<IActionResult> Dev(string username)
+        {
+            var developerId = _userManager.FindByNameAsync(username).Result.Id;
+            return Ok(developerId);
         }
 
         public IActionResult Privacy()
