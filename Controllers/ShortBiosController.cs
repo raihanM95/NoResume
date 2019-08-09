d8cf66d17c0091531df7659d1bb5d866f6c88749
shunjid <shunjid1862@gmail.com>
2019-08-09 10:54:58 +0600

    Optimized ShortBios

--- Controllers/ShortBiosController.cs
+++ Controllers/ShortBiosController.cs
@@ -25,54 +25,6 @@ namespace NoResume.Controllers
             return _userManager.GetUserId(HttpContext.User);
         }
         
-
-        // GET: ShortBios
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.ShortBios.ToListAsync());
-        }
-
-        // GET: ShortBios/Details/5
-        public async Task<IActionResult> Details(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var shortBio = await _context.ShortBios
-                .FirstOrDefaultAsync(m => m.DeveloperId == id);
-            if (shortBio == null)
-            {
-                return NotFound();
-            }
-
-            return View(shortBio);
-        }
-
-        // GET: ShortBios/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: ShortBios/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("DeveloperId,ShortDescription,CurrentCity,IsAvailableForJob")] ShortBio shortBio)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(shortBio);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(shortBio);
-        }
-
-        
         // GET: ShortBios/Edit/5
         [Authorize]
         
@@ -99,8 +51,6 @@ namespace NoResume.Controllers
         }
 
         // POST: ShortBios/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public JsonResult Edit(string id, [Bind("DeveloperId,ShortDescription,CurrentCity,IsAvailableForJob")] ShortBio shortBio)
@@ -133,36 +83,6 @@ namespace NoResume.Controllers
             return null;
         }
         
-        
-        // GET: ShortBios/Delete/5
-        public async Task<IActionResult> Delete(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var shortBio = await _context.ShortBios
-                .FirstOrDefaultAsync(m => m.DeveloperId == id);
-            if (shortBio == null)
-            {
-                return NotFound();
-            }
-
-            return View(shortBio);
-        }
-
-        // POST: ShortBios/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            var shortBio = await _context.ShortBios.FindAsync(id);
-            _context.ShortBios.Remove(shortBio);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
         private bool ShortBioExists(string id)
         {
             return _context.ShortBios.Any(e => e.DeveloperId == id);
