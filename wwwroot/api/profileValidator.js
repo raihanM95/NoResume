83039463c557baf22c63858b1d35d7568440c0ec
shunjid <shunjid1862@gmail.com>
2019-08-18 14:40:38 +0600

    fixed Postloader Bug at authorized pages

--- wwwroot/api/profileValidator.js
+++ wwwroot/api/profileValidator.js
@@ -1,72 +1,57 @@
+var requestToCodeForces;
+var requestToGitHub;
+var requestToUva;
+var gitHubApiURL = "https://api.github.com/users/";
+var uva_api_url = 'https://uhunt.onlinejudge.org/api/';
+var cf_api_url = 'https://codeforces.com/api/';
+
 $(document).ready(function(){
-    var loaderIntermediate = $('#postLoader');
-    loaderIntermediate.hide();
-    
+    var postLoader = $('#postLoader');
+    postLoader.hide();
+
     $("#formWorkingProfile").submit(function(e){
-        loaderIntermediate.show();
         e.preventDefault();
-        
-        var isGithubPermissible = false;
-        var isCFPermissible = false;
+        postLoader.show();
+        var githubUsername = $('#GithubUsername').val();
+        var cf_handle = $('#CodeforcesUsername').val();
+        var uvaHandle = $('#UhuntUsername').val();
 
-        var gitUna = $('#GithubUsername').val();
-        var cfUna = $('#CodeforcesUsername').val();
-
-        $.ajaxSetup({
-            async: false
+        requestToCodeForces = $.get(cf_api_url + 'user.status', { handle: cf_handle }, function (data, status) {
+            requestToUva = $.get(uva_api_url + 'uname2uid/' + uvaHandle, function (uvaData, uvaStatus) {
+               if(uvaData !== 0){
+                   requestToGitHub = $.get(gitHubApiURL + githubUsername, function (userInformation, userStatus) {
+                       $.post('', $('#formWorkingProfile').serialize(), function (response) {
+                           if(response != null || response !== "" || response !== "null"){
+                               M.toast({
+                                   html: 'Data updated Successfully',
+                                   classes: 'green darken-1 rounded'
+                               });
+                           }else{
+                               M.toast({
+                                   html: 'Something went wrong !',
+                                   classes: 'red darken-4 rounded'
+                               });
+                           }
+                       });
+                   }).fail(function (xhr, status) {
+                       if (status != 'abort') showUserDefinedToast("GitHub username is invalid","red darken-1 rounded");
+                   });
+               }else{
+                   showUserDefinedToast("UVA username is invalid", "red darken-1 rounded");
+               }
+           });
+        }).fail(function(xhr, status) {
+            if (status != 'abort') showUserDefinedToast("Codeforces username is invalid","red darken-1 rounded");
+        })
+        .always(function() {
+            postLoader.hide();
         });
-
-        if(gitUna.trim() === ""){
-            isGithubPermissible = true;
-        }else{
-            $.ajax({
-                url: "https://api.github.com/users/"+$('#GithubUsername').val(),
-                async: false,
-                dataType: 'json',
-                success: function(json, textStatus, xhr){
-                    if(xhr.status === 200){
-                        isGithubPermissible = true;
-                    }
-                }
-            });
-        }
-
-        if(cfUna.trim() === ""){
-            isCFPermissible = true;
-        }else{
-            $.ajax({
-                url: "https://codeforces.com/api/user.info?handles="+$('#CodeforcesUsername').val(),
-                async: false,
-                dataType: 'json',
-                success: function(json){
-                    if(json.status === "OK"){
-                        isCFPermissible = true;
-                    }
-                }
-            });
-        }
-        
-        if(isGithubPermissible === false || isCFPermissible === false){
-            M.toast({
-                html: 'Invalid Username',
-                classes: 'red darken-1 rounded'
-            });
-            loaderIntermediate.hide();
-        }else{
-            $.post('', $('#formWorkingProfile').serialize(), function (response) {
-                if(response != null || response !== "" || response !== "null"){
-                    M.toast({
-                        html: 'Data updated Successfully',
-                        classes: 'green darken-1 rounded'
-                    });
-                }else{
-                    M.toast({
-                        html: 'Something went wrong !',
-                        classes: 'red darken-4 rounded'
-                    });
-                }
-            });
-            loaderIntermediate.hide();
-        }
-    });
+    })
 });
+
+function showUserDefinedToast(message, style) {
+    M.toast({
+        html: message,
+        classes: style
+    });
+}
